4a04523534b788c1b17069e118b1314a6f80422b
Dominique Foley <dfoleydev@gmail.com>
2022-12-20 10:53:35 -0500

    first commit

---
+++ frontend-vite/src/App.jsx
@@ -0,0 +1,15 @@
+import Counter from './components/Counter'
+import Footer from './components/Footer'
+import Gallery from './components/Gallery'
+import Hero from './components/Hero'
+
+function App() {
+    return (
+        <>
+            <Hero />
+            <Gallery />
+            <Footer />
+        </>
+    )
+}
+export default App
