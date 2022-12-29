0b2fbc11b0b4ce2493a25a2d932f93980fcae257
Dominique Foley <dfoleydev@gmail.com>
2022-12-28 23:23:30 -0500

    Working version

--- frontend-vite/src/App.jsx
+++ frontend-vite/src/App.jsx
@@ -3,6 +3,7 @@ import Footer from './components/Footer'
 import Gallery from './components/Gallery'
 import Heros from './components/Heros'
 import Nav from './components/Nav'
+import Pictures from './components/Pictures'
 
 function App() {
     return (
