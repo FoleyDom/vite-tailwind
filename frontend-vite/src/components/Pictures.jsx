15ff1f9da4044b6bc590395a17ac5dfb3b189edb
Dominique Foley <dfoleydev@gmail.com>
2023-01-22 12:02:21 -0500

    Replaced pictures that would not load

--- frontend-vite/src/components/Pictures.jsx
+++ frontend-vite/src/components/Pictures.jsx
@@ -1,33 +1,38 @@
+import footballingrass from '../assets/footballingrass.png'
+import throwfootball from '../assets/throwfootball.png'
+import players from '../assets/players.png'
+import footballsign from '../assets/footballsign.png'
+
 export default function Pictures() {
-    return (
-        <section class="text-gray-400 bg-gray-900 body-font">
-            <div class="container px-5 py-24 mx-auto flex flex-wrap">
-                <div class="flex flex-wrap md:-m-2 -m-1">
-                    <div class="flex flex-wrap">
-                        <div class="md:p-2 p-1 w-full">
-                            <img
-                                alt="gallery"
-                                class="w-full h-full object-cover object-center block"
-                                src="https://i.ibb.co/GtVDv2f/pexels-terrance-barksdale-10322267.webp"
-                            />
-                        </div>
-                        <div class="md:p-2 p-1 w-1/2">
-                            <img
-                                alt="gallery"
-                                class="w-full object-cover h-full object-center block"
-                                src="https://i.ibb.co/ysDhLPY/tinywow-flag-football-g898403da4-1920-10505174.webp"
-                            />
-                        </div>
-                        <div class="md:p-2 p-1 w-1/2">
-                            <img
-                                alt="gallery"
-                                class="w-full object-cover h-full object-center block"
-                                src="https://i.ibb.co/9bHHykV/tinywow-football-g12a87bc44-1920-10505293.webp"
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
+	return (
+		<section class='text-gray-400 bg-gray-900 body-font'>
+			<div class='container px-5 py-24 mx-auto flex flex-wrap'>
+				<div class='flex flex-wrap md:-m-2 -m-1'>
+					<div class='flex flex-wrap'>
+						<div class='md:p-2 p-1 w-full'>
+							<img
+								alt='gallery'
+								class='w-full h-full object-cover object-center block'
+								src={footballsign}
+							/>
+						</div>
+						<div class='md:p-2 p-1 w-1/2'>
+							<img
+								alt='gallery'
+								class='w-full object-cover h-full object-center block'
+								src={throwfootball}
+							/>
+						</div>
+						<div class='md:p-2 p-1 w-1/2'>
+							<img
+								alt='gallery'
+								class='w-full object-cover h-full object-center block'
+								src={players}
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
